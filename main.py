8e6fd27b31caa8bd6088034d882d9e8d7d15bcf7
buggy <test@gmail.com>
2025-06-10 20:20:15 +0630

    walker prototype

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print("hello world")
+import os
+
+class Walker:
+    def __init__(self, base):
+        self.base = base
+
+    def file_wrapper(self, handler):
+        def wrapper(*args, **kwargs):
+            for root, dirs, files in os.walk(self.base):
+                for file in files:
+                    handler(os.path.join(root, file))
+        return wrapper
+
+wlk = Walker('.')
+
+X = 0
+
+@wlk.file_wrapper
+def test(x):
+    print(x)
+
+
+test()
+
