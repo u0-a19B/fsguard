172def48e36d4ca2960cbd604d579c532be04c1c
buggy <test@gmail.com>
2025-06-14 12:12:58 +0630

    Config parser added and tested

--- main.py
+++ main.py
@@ -1,18 +1,40 @@
 import os
 import hashlib
 import sys
-hash_buffer = ""
+import json
+
+file_hashed: dict[str,str] = {}
+dir_hashed: dict[str,str] = {}
+
+
+def Parser(file):
+    try:
+        f = open(file, 'r')
+        data = f.read()
+        f.close()
+        js = json.loads(data)
+        return js['f'], js['d']
+    except json.decoder.JSONDecodeError:
+        return None, None
+    except KeyError:
+        return None, None
+    except FileNotFoundError:
+        print(f"No such file {file}")
+        sys.exit(-1)
 
 class Walker:
+
     def __init__(self, base):
         self.base = base
+        self.hash_buffer: str = ""
 
-    def file_wrapper(self, handler):
-        def wrapper(*args, **kwargs):
-            for root, dirs, files in os.walk(self.base):
-                for file in files:
-                    handler(os.path.join(root, file))
-        return wrapper
+    def file_wrapper(self):
+        for root, dirs, files in os.walk(self.base):
+            for file in files:
+                self.hash_dir(os.path.join(root, file))
+
+    def hash_dir(self, f):
+        self.hash_buffer += Walker.hash_file(f)
 
     @staticmethod
     def hash_file(file):
@@ -20,21 +42,31 @@ class Walker:
             with open(file, 'rb') as data:
                 return hashlib.md5(data.read()).hexdigest()
         except FileNotFoundError:
+            print(f"No such file {file}")
             sys.exit(-1)
 
     @staticmethod
     def hash_string(h):
         return hashlib.md5(h).hexdigest()
 
-wlk = Walker('/home/buggy/fsguard')
-
 X = 0
 
-@wlk.file_wrapper
-def hash_dir(f):
-    global hash_buffer
-    hash_buffer += Walker.hash_file(f)
+def hash_init():
+    files, dirs = Parser(sys.argv[1])
+    if files == None:
+        print("Config decode error")
+        sys.exit(-2)
+    for f in files:
+        file_hashed[f] = Walker.hash_file(f)
+    for d in dirs: 
+        wlk = Walker(d)
+        wlk.file_wrapper()
+        dir_hashed[d] = hashlib.md5(wlk.hash_buffer.encode()).hexdigest()
 
 # print(Walker.hash_file('main.py'))
-hash_dir()
-print(Walker.hash_string(hash_buffer.encode()))
+# hash_dir()
+# print(Walker.hash_string(hash_buffer.encode()))
+# print(Parser(sys.argv[1]))
+hash_init()
+print(file_hashed)
+print(dir_hashed)
